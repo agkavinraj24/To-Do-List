b677aeede053093db012af5d37c0276895262aa3
Kavin <agkavinraj24@gmail.com>
2021-11-23 11:33:05 +0530

    To do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl:  './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'to-do-list';
+export class AppComponent 
+{
+    title = 'to-do-list';
+    isContent:boolean=true;
+    public items : any = [];
+    public newTask:any;
+    public addToList() {
+        if (this.newTask == '') 
+        {
+            this.isContent=false;
+        }
+        else 
+        {
+            this.items.push(this.newTask);
+            this.newTask = '';
+        }
+    }
 }
